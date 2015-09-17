dd782f70df3eb0b21da27652447ea1311375e29d
lucianoRM <luciano.rmna@gmail.com>
2015-09-17 15:38:17 -0300

    Added gtest to include folder. Modified cmake file, now gtest compiles on its own and 2 executables are compiled. fdrive executable inside src directory and test executable inside test folder. Added 2 test mocks inside test folder as an example

---
+++ src/test/test1.cpp
@@ -0,0 +1,8 @@
+
+
+
+#include "gtest/gtest.h"
+
+TEST(EqualityTest, UnosIguales) {
+  EXPECT_EQ(1, 1);
+}
---
+++ src/test/test2.cpp
@@ -0,0 +1,7 @@
+
+
+#include "gtest/gtest.h"
+
+TEST(EqualityTest2, DosIguales) {
+  EXPECT_EQ(2, 1);
+}
