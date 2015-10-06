ff5fd6477fe92318a7b57bf1eade0e52aff9a87a
martin_margonari <martinmargonari@gmail.com>
2015-10-06 14:41:37 -0300

    Agrego tests de Usuario

--- src/test/testUser.cpp
+++ src/test/testUser.cpp
@@ -37,90 +37,55 @@ TEST(SignUpTest, SignupAvailableEmail) {
     delete user;
 }
 
-/*
 TEST(SignUpTest, SignupTakenEmail){
     rocksdb::DB* db = openDatabase();
     if (! db) {
         return;
     }
 
-    std::string email = genRandomEmail();
-    User* user = new User(email,"password");
+    User* user = new User("emailTest","password");
+    user->signup(db);
 
-    EXPECT_EQ(true,user->signup(db));
-
-    User* user2= new User(email,"password");
+    User* user2= new User("emailTest","password");
     EXPECT_EQ(false,user2->signup(db));
+
+    db->Delete(rocksdb::WriteOptions(), "users.emailTest");
 
     delete db;
     delete user;
     delete user2;
-
 }
 
-TEST(SignUpTest, SignupNoEmail){
-    EXPECT_EQ(true,true);
-}
-
-TEST(LoadTest, loadSavedUser){
+TEST(SignInTest, SignInExisting){
     rocksdb::DB* db = openDatabase();
     if (! db) {
         return;
     }
 
-    std::string email = genRandomEmail();
-    User* user = new User(email,"password");
+    User* user = new User("emailTest","password");
     user->signup(db);
 
     EXPECT_EQ(true,user->load(db));
+
+    db->Delete(rocksdb::WriteOptions(), "users.emailTest");
 
     delete db;
     delete user;
 }
 
-TEST(LoadTest, loadNonExistentUser){
+TEST(SignInTest, SignInNonExisting){
     rocksdb::DB* db = openDatabase();
     if (! db) {
         return;
     }
 
-    std::string email = genRandomEmail();
-    User* user = new User(email,"password");
+    User* user = new User("emailTest","password");
 
     EXPECT_EQ(false,user->load(db));
 
     delete db;
     delete user;
-
 }
-/*
-TEST(LoadTest, loadUserDifferentEmail){
-
-    rocksdb::DB* db;
-    rocksdb::Options options;
-    options.create_if_missing = true;
-    rocksdb::Status status = rocksdb::DB::Open(options, "mockdb", &db);
-
-    //La db se abrio correctamente
-    if (!status.ok()){ std::cout << "Error DB:" << status.ToString() << std::endl; }
-
-
-    User* user1 = new User();
-    User* user2 = new User();
-    std::string email1 = genRandomEmail();
-    std::string email2 = genRandomEmail();
-    user1->setEmail(email1);
-    user2->setEmail(email2);
-    user1->signup(db);
-    user2->signup(db);
-
-    EXPECT_EQ(false,user1->load(db,email2));
-
-    delete db;
-    delete user1;
-    delete user2;
-
-}*/
 
 
 
