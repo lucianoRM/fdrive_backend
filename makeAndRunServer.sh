077af8ec2c6a6c6f0fd036f235ff4c26eb63b570
Agustín Santiago <agustin@persit.com.ar>
2015-10-08 14:57:47 -0300

    Optimizing loading server and tests.

--- makeAndRunServer.sh
+++ makeAndRunServer.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-cp /rocksdb/rocksdb/librocksdb.a src/
-rm -rf build/*
+
+env=$1
+if [ -z $env ]
+then
+    env="local"
+fi
+
+if [ -f "lastrun" ]
+then
+    read -r line < "lastrun"
+else
+    line=""
+fi
+
+if [ -z $line ] || [ $line != $env ]
+then
+    echo "Setting up rocksdb."
+    cp /rocksdb/rocksdb/librocksdb.a src/
+    rm -rf build/*
+fi
+echo $env > "lastrun"
+
 cd build
 cmake ../src/
 make
--- runTestsInDocker.sh
+++ runTestsInDocker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-#docker build -t fdrive_backend .
-docker stop backend
-docker rm backend
-docker run -i -t -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/makeAndRunTests.sh
+result=`docker ps -a | grep backend`
+if [ ! -z "$result" ]
+then
+    docker rm backend
+fi
+docker run -i -t -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/makeAndRunTests.sh docker
