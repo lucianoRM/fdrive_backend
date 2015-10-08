05884c9b35d859fe0f1d9b7cf9462c317a74f86b
Agustín Santiago <agustin@persit.com.ar>
2015-10-08 16:18:05 -0300

    Removed two unnecessary tests and made some methods in User private.

--- src/user.h
+++ src/user.h
@@ -26,24 +26,27 @@ private:
     std::string hashed_password;
     //std::vector<> tokens;
     bool checkIfExisting(rocksdb::DB* db, std::string* value);
+    bool checkPassword(std::string password);
 
 public:
 
     User();
+
+    std::string hashPassword(std::string password); // Left for testing purposes
     
     std::string getEmail();
-    
-    std::string hashPassword(std::string password);
-    void signup(rocksdb::DB* db);
     void setEmail (std::string email);
+
     void setPassword (std::string password);
+
+    void signup(rocksdb::DB* db);
     bool signin(std::string password);
-    bool checkPassword(std::string password);
-    bool save(rocksdb::DB* db);
+
 	bool addToken(rocksdb::DB* db, std::string token);
     void checkToken(rocksdb::DB* db,std::string token); //Checks if token is associated with user.
     
     static User* load(rocksdb::DB* db, std::string email);
+    bool save(rocksdb::DB* db);
     
 };
 
