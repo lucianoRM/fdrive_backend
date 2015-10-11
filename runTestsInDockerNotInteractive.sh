24b3ae16a26b7546d1696c218b23a29a034dfe78
Agustín Santiago <agustin@persit.com.ar>
2015-10-10 21:42:17 -0300

    debugging jenkins

---
+++ runTestsInDockerNotInteractive.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+result=`docker ps -a | grep backend`
+if [ ! -z "$result" ]
+then
+    docker rm backend
+fi
+docker run -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/makeAndRunTests.sh docker
