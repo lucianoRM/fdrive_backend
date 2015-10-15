d2eca16f28595ad539fa3fbfef94d26c71fd505f
Agustín Santiago <agustin@persit.com.ar>
2015-10-15 20:32:37 -0300

    Fixing python tests execution.

--- runPythonTests.sh
+++ runPythonTests.sh
@@ -1,7 +1,8 @@
-./runServerInDockerNotInteractive.sh &
-PID=$!
-sleep 7
-python -m unittest discover -s functional_tests -p "*.py"
-result=$?
+#!/usr/bin/env bash
 docker stop backend
-exit $result
+result=`docker ps -a | grep backend`
+if [ ! -z "$result" ]
+then
+    docker rm backend
+fi
+docker run -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/makeAndRunPythonTests.sh docker
