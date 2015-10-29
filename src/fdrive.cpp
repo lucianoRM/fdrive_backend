b12938d29cd4bc6eb4c41189c57e3a8c3e5aca6b
martin_margonari <martinmargonari@gmail.com>
2015-10-29 13:41:12 -0300

    Fixed style in fdrive.cpp

--- src/fdrive.cpp
+++ src/fdrive.cpp
@@ -4,22 +4,18 @@
 #define TOTAL_LISTENERS 100
 #define LISTENING_PORT "8000"
 
-
-int main(){
-
-
-	//Creates main server
-
+int main() {
+	// Creates main server.
 	Server* mainServer = new Server(LISTENING_PORT);
 
-	//Creates TOTAL_LISTENERS servers to handle up to TOTAL_LISTENERS connections at the same time and launches a thread for each one.
-	for(int i = 0; i < TOTAL_LISTENERS; i++){
+	// Creates TOTAL_LISTENERS servers to handle up to TOTAL_LISTENERS connections at the same time and launches a thread for each one.
+	for (int i = 0; i < TOTAL_LISTENERS; i++) {
 		Server* server = new Server(LISTENING_PORT);
 		server->copyListeners(mainServer);
 		server->listenOnThread();
 	}
 
-	//Prevents from closing
+	// Prevents from closing.
 	while(true) usleep(10000);
 
 	return 0;
