ee8a519901b3a6d76c4398aba900281ef676a6be
Agustín Santiago <agustin@persit.com.ar>
2015-10-29 21:38:59 -0300

    Debugging

--- runTestsInDocker.sh
+++ runTestsInDocker.sh
@@ -5,5 +5,17 @@ if [ ! -z "$result" ]
 then
     docker rm backend
 fi
-docker run -i -t -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/utils/makeAndRunTests.sh docker
+if [[ ($# -eq 1) && ($1 = "-d") ]]
+then
+    detached=1
+else
+    detached=0
+fi
+if [[ detached -eq 1 ]]
+then
+    flags="-d"
+else
+    flags="-i -t"
+fi
+docker run $flags -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/utils/makeAndRunTests.sh docker
 docker stop backend
