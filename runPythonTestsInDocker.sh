e7c5e1c2593208aae5180e1c16857560f38cbc91
Agustín Santiago <agustin@persit.com.ar>
2015-10-29 21:27:37 -0300

    Organizing util scripts & testing jenkins.

--- runPythonTestsInDocker.sh
+++ runPythonTestsInDocker.sh
@@ -5,4 +5,5 @@ if [ ! -z "$result" ]
 then
     docker rm backend
 fi
-docker run -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/utils/makeAndRunPythonTests.sh docker
+docker run -i -t -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/utils/makeAndRunPythonTests.sh docker
+docker stop backend
--- runTestsInDocker.sh
+++ runTestsInDocker.sh
@@ -5,4 +5,5 @@ if [ ! -z "$result" ]
 then
     docker rm backend
 fi
-docker run -d -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/utils/makeAndRunTests.sh docker
+docker run -i -t -p 8000:8000 --name backend -v "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )":/backend fdrive_backend /backend/utils/makeAndRunTests.sh docker
+docker stop backend
