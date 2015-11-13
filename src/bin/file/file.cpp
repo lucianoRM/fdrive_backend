838364f5dfa1eff10719a650227cfb7be44c7824
martin_margonari <martinmargonari@gmail.com>
2015-11-13 18:33:40 -0300

    Configuring remove of files

--- src/bin/file/file.cpp
+++ src/bin/file/file.cpp
@@ -249,30 +249,31 @@ void File::checkIfUserIsOwner(std::string email) {
 
 void File::eraseFromUser(rocksdb::DB* db, std::string user, std::string path) {
     struct metadata* metadata = (*this->versions)[this->lastVersion]->getMetadata();
-    //if (user.compare(this->owner)) {
-        //for (std::string sharedUser : *this->users) {
-          //  this->deleteFromUser(db, sharedUser, "shared");
-        //}
-        //this->deleteFromUser(db, user, path);
-        Folder* folder = NULL;
-        try {
-            folder = Folder::load(db, user, "trash");
-            folder->addFile(this->id, metadata->name + "." + metadata->extension);
-            folder->save(db);
-            delete folder;
-            folder = Folder::load(db, user, path);
-            folder->removeFile(this->getId());
-            folder->save(db);
-        } catch (std::exception& e) {
-            if (folder != NULL) delete folder;
-            throw;
+    if (user.compare(this->owner) == 0) {
+        for (std::string sharedUser : *this->users) {
+            this->eraseFromUser(db, sharedUser, "shared");
         }
+    }
+    //this->deleteFromUser(db, user, path);
+    Folder* folder = NULL;
+    try {
+        folder = Folder::load(db, user, "trash");
+        folder->addFile(this->id, metadata->name + "." + metadata->extension);
+        folder->save(db);
         delete folder;
-        this->users->clear();
-    //} else {
-        //this->deleteFromUser(db, user, "shared");
-        this->users->remove(user);
-    //}
+        folder = Folder::load(db, user, path);
+        folder->removeFile(this->getId());
+        folder->save(db);
+    } catch (std::exception& e) {
+        if (folder != NULL) delete folder;
+        throw;
+    }
+    delete folder;
+    //this->users->clear();
+//} else {
+    //this->deleteFromUser(db, user, "shared");
+    this->users->remove(user);
+//}
 }
 /*
 void File::deleteFromUser(rocksdb::DB* db, std::string user, std::string path) {
